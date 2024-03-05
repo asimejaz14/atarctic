c8a8a45f410171aa502b7f189c894af931e7f8ec
Asim Ejaz <asim.ejaz14@gmail.com>
2024-03-05 18:04:37 +0500

    is_restricted field added user model

--- user/models.py
+++ user/models.py
@@ -7,6 +7,7 @@ class User(DateTimeLog):
     username = models.CharField(max_length=128, null=True, blank=True)
     password = models.CharField(max_length=128)
     status = models.CharField(max_length=1, choices=STATUS, default=Status.ACTIVE)
+    is_restricted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
